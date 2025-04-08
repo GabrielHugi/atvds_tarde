b11010ff0470e7fec718d9ba9a323e941b3e7040
Gabriel Hugi <gabrielhugi2@gmail.com>
2025-04-08 14:03:57 -0300

    structure

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanf;
+        
+        Quadrado teste = new Quadrado(3.0);
+        System.out.println(teste.getLado());
+        
     }
 }
---
+++ Quadrado.java
@@ -0,0 +1,15 @@
+
+public class Quadrado {
+    private Double lado;
+    private Double posicao;
+
+    public Double getLado() {
+        return this.lado;
+    }
+    public Quadrado(Double lado, Double posicao) {
+
+    }
+    public Quadrado(Double lado) {
+
+    }
+}
